181366b0281bb4180522ab836fe1377b88edab64
David Mezza <d.mezza@zunocarbon.com>
2025-06-14 19:31:11 +0300

    :rocket: Adding JwT Util code

--- auth-service/src/main/java/com/elinonga/auth_service/security/JwtUtil.java
+++ auth-service/src/main/java/com/elinonga/auth_service/security/JwtUtil.java
@@ -1,5 +1,49 @@
 package com.elinonga.auth_service.security;
 
+import com.elinonga.auth_service.user.User;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
-    
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("role", user.getRole())
+                .claim("user_id", user.getId().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
