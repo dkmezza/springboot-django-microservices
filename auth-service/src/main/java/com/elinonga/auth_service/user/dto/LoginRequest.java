a659bbaffd0ae39c414aac74197fed6402333c72
David Mezza <d.mezza@zunocarbon.com>
2025-06-14 18:29:54 +0300

    :fire: Adding a Login Request

--- auth-service/src/main/java/com/elinonga/auth_service/user/dto/LoginRequest.java
+++ auth-service/src/main/java/com/elinonga/auth_service/user/dto/LoginRequest.java
@@ -1,5 +1,16 @@
 package com.elinonga.auth_service.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
-    
+    private String email;
+    private String password;
 }
+
