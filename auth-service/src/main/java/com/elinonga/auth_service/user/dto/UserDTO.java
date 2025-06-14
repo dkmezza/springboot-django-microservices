cfff74b02925d0c59ec8a33691fc5bf7ea00c7ff
David Mezza <d.mezza@zunocarbon.com>
2025-06-14 18:32:28 +0300

    :construction: Adding UserDTO

--- auth-service/src/main/java/com/elinonga/auth_service/user/dto/UserDTO.java
+++ auth-service/src/main/java/com/elinonga/auth_service/user/dto/UserDTO.java
@@ -1,5 +1,18 @@
 package com.elinonga.auth_service.user.dto;
 
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
-    
+    private UUID id;
+    private String email;
+    private String role;
 }
