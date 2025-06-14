6ee4eda2ef4f3441132794344cc384c3c0a93396
David Mezza <d.mezza@zunocarbon.com>
2025-06-14 16:59:08 +0300

    :zap: Adding User Entity

--- auth-service/src/main/java/com/elinonga/auth_service/user/User.java
+++ auth-service/src/main/java/com/elinonga/auth_service/user/User.java
@@ -1,5 +1,34 @@
 package com.elinonga.auth_service.user;
+import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role; // "admin", "user"
 }
+
