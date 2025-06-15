18cc9db58c2abe38449fa2bcef2c02adf677ce4f
David Mezza <d.mezza@zunocarbon.com>
2025-06-15 13:02:45 +0300

    :art: Added Tenant model

--- auth-service/src/main/java/com/elinonga/auth_service/tenant/Tenant.java
+++ auth-service/src/main/java/com/elinonga/auth_service/tenant/Tenant.java
@@ -1,5 +1,32 @@
 package com.elinonga.auth_service.tenant;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tenant {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String status; // e.g., "active", "inactive"
 }
