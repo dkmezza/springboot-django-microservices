9d75683977d5fa7354dc537e2d7643ddae2765f8
David Mezza <d.mezza@zunocarbon.com>
2025-06-15 12:52:28 +0300

    :fire: Adding tables in User entity class + minor update in application.properties

--- auth-service/src/main/java/com/elinonga/auth_service/user/User.java
+++ auth-service/src/main/java/com/elinonga/auth_service/user/User.java
@@ -1,10 +1,12 @@
 package com.elinonga.auth_service.user;
+
 import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,23 +14,22 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User {
-
     @Id
     @GeneratedValue
     private UUID id;
-
+    
     @Column(unique = true, nullable = false)
     private String email;
-
+    
     @Column(nullable = false)
     private String password;
-
+    
     private String role; // "admin", "user"
 }
-
