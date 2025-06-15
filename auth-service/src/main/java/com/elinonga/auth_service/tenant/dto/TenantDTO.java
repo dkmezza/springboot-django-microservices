d3a36c5658390230f4d1754ecb1074d8c3c237ac
David Mezza <d.mezza@zunocarbon.com>
2025-06-15 13:13:31 +0300

    :construction: Added Tenant DTO

--- auth-service/src/main/java/com/elinonga/auth_service/tenant/dto/TenantDTO.java
+++ auth-service/src/main/java/com/elinonga/auth_service/tenant/dto/TenantDTO.java
@@ -1,5 +1,19 @@
 package com.elinonga.auth_service.tenant.dto;
 
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TenantDTO {
-    
+    private UUID id;
+    private String name;
+    private String status;
+    private UUID createdBy;
 }
