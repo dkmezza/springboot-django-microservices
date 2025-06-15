28cbe7bcab94c360e410310478ecc9468ec71919
David Mezza <d.mezza@zunocarbon.com>
2025-06-15 13:08:50 +0300

    :zap: Added created_by field in Tenant model + table name

--- auth-service/src/main/java/com/elinonga/auth_service/tenant/Tenant.java
+++ auth-service/src/main/java/com/elinonga/auth_service/tenant/Tenant.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "tenants")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -28,5 +30,9 @@ public class Tenant {
     private String name;
 
     @Column(nullable = false)
-    private String status; // e.g., "active", "inactive"
+    private String status;
+
+    @Column(name = "created_by", nullable = false)
+    private UUID createdBy;
 }
+
